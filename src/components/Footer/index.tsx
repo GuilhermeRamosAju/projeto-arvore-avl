94d1f467e1a8d90ef31b73ffc17784a20b39bd78
Anna Nayara Lira <93208656+nayara-al@users.noreply.github.com>
2024-06-01 18:54:15 -0300

    feat: adicionando logo unit

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import UnitLogo from "../../assets/unit.png"
 const Footer = () => {
   return (
-    <footer>
-      <p className="text-sm text-gray-400">
+    <footer className="flex justify-center items-center gap-4">
+      <img src={UnitLogo} className="w-12 h-auto" alt="logo unit"/>
+      <p className="text-sm text-gray-400 m-0">
         Criado por Anna Nayara, Guilherme Ramos, João Arthur e Pedro Arthur
       </p>
     </footer>
